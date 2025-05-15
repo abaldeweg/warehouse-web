a96551317873d620757a48bfabfd051c925d920b
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-05-15 09:56:45 +0000

    implement authentication guard in router to restrict access to protected routes

--- packages/app/src/router/index.ts
+++ packages/app/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuth } from '@/composables/useAuth.ts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,31 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue'),
     },
     {
+      path: '/orders',
+      name: 'orders',
+      component: () => import('../views/OrderListView.vue'),
+    },
+    {
+      path: '/orders/:id',
+      name: 'order.detail',
+      component: () => import('../views/OrderDetailView.vue'),
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: () => import('../views/AccountView.vue'),
+    },
+    {
+      path: '/password',
+      name: 'password',
+      component: () => import('../views/PasswordView.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
       path: '/:pathMatch(.*)',
       redirect: { name: 'home' },
     },
@@ -18,6 +44,19 @@ const router = createRouter({
   scrollBehavior() {
     return { top: 0 }
   },
+})
+
+router.beforeEach(async (to) => {
+  if (to.name === 'login') {
+    return
+  }
+
+  const { isUserAuthenticated } = useAuth()
+
+  const authenticated = await isUserAuthenticated()
+  if (!authenticated) {
+    router.push({ name: 'login' })
+  }
 })
 
 export default router
