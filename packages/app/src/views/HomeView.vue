c5f5a06663bf22c655cd7c62757253b85026787e
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-05-15 10:02:29 +0000

    fix: update HomeView to use i18n for title and remove Welcome component

--- packages/app/src/views/HomeView.vue
+++ packages/app/src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-import Welcome from '@/components/Welcome.vue'
 import { useI18n } from 'vue-i18n'
 import { useHead } from '@unhead/vue'
 
 const { t } = useI18n()
 
 useHead({
-  title: 'Welcome',
+  title: t('Welcome'),
 })
 </script>
 
 <template>
   <BContainer size="m">
-    <Welcome :msg="t('welcome')" />
+    Welcome to the app!
   </BContainer>
 </template>
