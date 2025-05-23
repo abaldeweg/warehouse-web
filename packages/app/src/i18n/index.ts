29ce048bcaa8c2dc10248c65c3575bc5a8d0597b
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-05-23 20:15:05 +0000

    refactor: update comments for clarity on translation key naming conventions

--- packages/app/src/i18n/index.ts
+++ packages/app/src/i18n/index.ts
@@ -5,7 +5,7 @@ const modules = import.meta.glob('./locales/*.json')
 // Function to extract the name from the file path
 const getName = (data: string): string => data.replace(/.\/locales\/(.*).json/, '$1')
 
-// Initialize an empty messages object to store the translations
+// Use camelCase or snake_case as key names, kebab-case could lead into issues
 const messages: { [key: string]: any } = {}
 for (const path in modules) {
   modules[path]().then((mod) => {
@@ -13,8 +13,6 @@ for (const path in modules) {
   })
 }
 
-// Create and export the i18n instance with the specified options
-// Use camelCase or snake_case as key names, kebab-case could lead into issues
 export default createI18n({
   legacy: false,
   locale: navigator.language,
