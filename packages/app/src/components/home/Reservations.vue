171ac25a832b8953470dab61fda12b29b9ea42a5
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-05-24 23:16:32 +0000

    feat: add Reservations component to HomeView for displaying reservation statistics

---
+++ packages/app/src/components/home/Reservations.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { useOrder } from '@/composables/useOrder'
+import { computed, onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useDate } from '@/composables/useDate'
+
+const { t } = useI18n()
+
+const { orders, list } = useOrder()
+onMounted(list)
+
+const { isOlderThan } = useDate()
+
+const countReservations = computed(() => {
+  return orders.value?.length || 0
+})
+
+const countOpenReservations = computed(() => {
+  return orders.value?.filter((order) => order.open === true).length || 0
+})
+
+const countOutdatedReservations = computed(() => {
+  return orders.value?.filter((order) => isOlderThan(14, order.createdAt)).length || 0
+})
+</script>
+
+<template>
+  <BContainer size="m">
+    <h2>{{ t('reservations') }}</h2>
+  </BContainer>
+
+  <BContainer size="m">
+    <BFlex gap="20px">
+      <div class="card">
+        <div class="card_number"><BMaterialIcon :size="50">storefront</BMaterialIcon></div>
+      </div>
+      <div class="card">
+        <div class="card_number">{{ countReservations }} in total</div>
+      </div>
+      <div class="card">
+        <div class="card_number isHighlighted">{{ countOpenReservations }} new</div>
+      </div>
+      <div class="card">
+        <div class="card_number isHighlighted">{{ countOutdatedReservations }} outdated</div>
+      </div>
+    </BFlex>
+  </BContainer>
+</template>
+
+<style scoped>
+.card {
+  flex: 1;
+  border: 1px solid var(--color-neutral-02);
+  border-radius: 20px;
+  padding: 20px;
+  text-align: center;
+}
+.card_number {
+  font-size: 2rem;
+  font-weight: bold;
+}
+.card_number.isHighlighted {
+  color: var(--color-primary-10);
+}
+.card_title {
+  margin-top: 5px;
+  color: var(--color-neutral-08);
+  font-size: 0.9rem;
+}
+</style>
--- packages/app/src/views/HomeView.vue
+++ packages/app/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { useI18n } from 'vue-i18n'
 import { useHead } from '@unhead/vue'
 import Stats from '@/components/home/Stats.vue'
+import Reservations from '@/components/home/Reservations.vue'
 
 const { t } = useI18n()
 
@@ -15,5 +16,6 @@ useHead({
     <h1>{{ t('welcome_to_warehouse') }}</h1>
   </BContainer>
 
+  <Reservations />
   <Stats />
 </template>
