6a79324fb3b6222c6b7063bfbf01204f37b49210
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-07-03 23:46:41 +0000

    refactor: remove unused loadLocale function from i18n index

--- packages/app/src/i18n/index.ts
+++ packages/app/src/i18n/index.ts
@@ -2,11 +2,6 @@ import { createI18n } from 'vue-i18n'
 import en from './locales/en.json'
 import de from './locales/de.json'
 
-const loadLocale = async (locale: string) => {
-  const messages = await import(`./locales/${locale}.json`)
-  return messages.default
-}
-
 const messages = {
   en,
   de,
