b512ad1c35802e52f2f62f92ce3ff52a69dd6dae
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2026-04-17 12:26:15 +0000

    fix: update navigation routes to use string literals for consistency

--- packages/app/src/components/AppPanel.vue
+++ packages/app/src/components/AppPanel.vue
@@ -37,14 +37,12 @@ const shop: string = import.meta.env.VITE_SHOP
     </template>
 
     <BNavigation>
-      <BNavigationItem :route="{ name: 'home' }" icon="home">{{ $t('home') }}</BNavigationItem>
+      <BNavigationItem :route="'/home'" icon="home">{{ $t('home') }}</BNavigationItem>
       <BNavigationItem :route="catalog" icon="menu_book">{{ $t('catalog') }}</BNavigationItem>
-      <BNavigationItem :route="{ name: 'reservation' }" icon="shopping_cart">{{
+      <BNavigationItem :route="'/reservation'" icon="shopping_cart">{{
         $t('reservations')
       }}</BNavigationItem>
-      <BNavigationItem :route="{ name: 'settings' }" icon="settings">{{
-        $t('settings')
-      }}</BNavigationItem>
+      <BNavigationItem :route="'/settings'" icon="settings">{{ $t('settings') }}</BNavigationItem>
       <BNavigationItem :route="shop" icon="store">{{ $t('shop') }}</BNavigationItem>
       <BNavigationItem :route="'/help'" icon="help">{{ $t('help') }}</BNavigationItem>
     </BNavigation>
